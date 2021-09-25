ea0ecbc2dedbf5f03d3de519517c48b479684fbf
Sheyzouh <83259991+SheyzouhDev@users.noreply.github.com>
2021-09-25 09:52:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -303,23 +303,3 @@ client.on('message', async (message, guild) => {
         })
     }
 })
-
-/*const voiceCollection = new Discord.Collection()
-
-client.on('voiceStateUpdate', async (oldState, newState) => {
-
-    const user = await client.users.fetch(newState.id)
-    const member = newState.guild.member(user)
-
-    if (!oldState.channel && newState.channel.id === '866778374835011584') {
-        const channel = await newState.guild.channels.create(user.tag, {
-            type: 'voice',
-            parent: newState.channel.parent
-        })
-        member.voice.setChannel(channel)
-        voiceCollection.set(user.id, channel.id)
-    } else if (!newState.channel) {
-        if (oldState.channelID === voiceCollection.get(newState.id)) 
-        return oldState.channel.delete()
-    }
-})*/
